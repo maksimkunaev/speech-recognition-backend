594baaa5944f5e56e7ef228a1105769a5bdef29b
random <termuovex@gmail.com>
2023-02-05 19:01:54 +0100

    fix: add runtime error exception

--- speech_recognition.py
+++ speech_recognition.py
@@ -3,7 +3,10 @@ import whisper
 model = whisper.load_model("base")
 
 
-def recognize(audio_paths):
-    result = model.transcribe(audio_paths[0], language="en")
-
-    return {'transcript': result["text"]}
+def recognize(audio_paths, language="en"):
+    try:
+        result = model.transcribe(audio_paths[0], language=language)
+        return {'transcript': result["text"]}
+    except RuntimeError as e:
+        print("RuntimeError: ", e)
+        return {'error': "An exception occurred", 'transcript': ""}
